4fa8bbdbee7b601979163344eaccb65b4effe772
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-09-25 02:49:27 -0300

    FEAT: user service

--- src/main/java/com/llm/hermes/service/UserService.java
+++ src/main/java/com/llm/hermes/service/UserService.java
@@ -1,2 +1,58 @@
-package com.llm.hermes.service;public class UserService {
+package com.llm.hermes.service;
+
+
+import com.llm.hermes.dto.UserDTO;
+import com.llm.hermes.entity.User;
+import com.llm.hermes.exception.ObjectNotFoundException;
+import com.llm.hermes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("The current user cannot be founded."));
+    }
+
+    public User create(User user){
+        return userRepository.insert(user);
+    }
+
+    public User update(User user){
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return userRepository.save(newUser);
+    }
+
+    public void updateData(User newUser, User user){
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+    }
+
+    public void delete(String id){
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return User.builder()
+                .name(userDTO.getName())
+                .email(userDTO.getEmail())
+                .id(userDTO.getId())
+                .build();
+    }
+
 }
