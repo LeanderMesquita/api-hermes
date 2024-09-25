18863220bf3ff0d7fa3878852ac48d72dca99ebc
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-09-25 04:00:19 -0300

    FEAT: exception handler

--- src/main/java/com/llm/hermes/core/exception/ExceptionHandler.java
+++ src/main/java/com/llm/hermes/core/exception/ExceptionHandler.java
@@ -1,2 +1,17 @@
-package com.llm.hermes.core.exception;public class ExceptionHandler {
+package com.llm.hermes.core.exception;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<MappedError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        MappedError error = new MappedError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Not founded", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
