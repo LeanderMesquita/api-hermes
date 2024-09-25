fa54ef96ae4bf4996ce7dea98dcc6123ce493f5a
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-09-25 03:00:33 -0300

    FEAT: post service

---
+++ src/main/java/com/llm/hermes/core/service/PostService.java
@@ -0,0 +1,33 @@
+package com.llm.hermes.core.service;
+
+import com.llm.hermes.core.entity.Post;
+import com.llm.hermes.core.exception.ObjectNotFoundException;
+import com.llm.hermes.core.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("The current post cannot be founded."));
+    }
+
+    public List<Post> findByTitle(String text){
+        return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
+    }
+
+}
