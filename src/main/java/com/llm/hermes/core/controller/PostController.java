1651693f7c490d980f4f96e6d11099e6242b62b6
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-09-25 03:50:45 -0300

    FEAT: post controller

--- src/main/java/com/llm/hermes/core/controller/PostController.java
+++ src/main/java/com/llm/hermes/core/controller/PostController.java
@@ -1,2 +1,45 @@
-package com.llm.hermes.core.controller;public class PostController {
+package com.llm.hermes.core.controller;
+
+
+import com.llm.hermes.core.entity.Post;
+import com.llm.hermes.core.service.PostService;
+import com.llm.hermes.core.util.URL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/view/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        String decodedText = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(decodedText);
+        return ResponseEntity.ok().body(list);
+    }
+
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate){
+
+        String decodedText = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(decodedText, min, max);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
