b8aab81b902293ea457e7a56e107cdc95617e603
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-09-25 02:15:56 -0300

    FEAT: entities

--- src/main/java/com/llm/hermes/entity/Post.java
+++ src/main/java/com/llm/hermes/entity/Post.java
@@ -1,2 +1,30 @@
-package com.llm.hermes.entity;public class Post {
+package com.llm.hermes.entity;
+
+import com.llm.hermes.dto.AuthorDTO;
+import com.llm.hermes.dto.CommentDTO;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Builder
+@Document
+public class Post implements Serializable {
+
+    @Id
+    private String id;
+    private String title;
+    private String body;
+    private Date date;
+
+    private AuthorDTO author;
+    private List<CommentDTO> comments;
 }
--- src/main/java/com/llm/hermes/entity/User.java
+++ src/main/java/com/llm/hermes/entity/User.java
@@ -1,2 +1,26 @@
-package com.llm.hermes.entity;public class User {
+package com.llm.hermes.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(exclude = {"id", "name"})
+@Document(collection = "user")
+public class User  implements Serializable {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts;
 }
