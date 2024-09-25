279d4af2c635e820009a2cef2ad7a70ef4d324a9
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-09-25 02:16:37 -0300

    FEAT: entities repositories

--- src/main/java/com/llm/hermes/repository/UserRepository.java
+++ src/main/java/com/llm/hermes/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.llm.hermes.repository;public interface UserRepository {
+package com.llm.hermes.repository;
+
+
+import com.llm.hermes.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
 }
